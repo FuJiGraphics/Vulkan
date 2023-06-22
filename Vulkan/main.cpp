8587560bfaf5f8a26c8230854fdd151be0ceb935
test <chlcjfwls1@naver.com>
2023-06-22 20:44:53 +0900

    GLFW initialized and create windows

--- Vulkan/main.cpp
+++ Vulkan/main.cpp
@@ -1,4 +1,9 @@
-#include <vulkan/vulkan.h>
+// Code reference by Vulkan Tutorial
+// https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Base_code
+
+// This header includes the Vulkan header automatically.
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h> 
 
 #include <iostream>
 #include <stdexcept>
@@ -7,43 +12,69 @@
 class HelloTriangleApplication
 {
 public:
-    void run()
-    {
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
 
 private:
-    void initVulkan() 
-    {
-    
-    }
+	void initWindow()
+	{
+		// These built-in functions are the first step to the necessary. 
+		glfwInit();
+		// GLFW was originally desinged to create an OpenGL context.
+		// we need to tell it to not create an OpenGL context with a subsequent call
+		glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
+		// to Disable the configuration for a resize windows. 
+		glfwWindowHint( GLFW_RESIZABLE, GLFW_FALSE );
 
-    void mainLoop()
-    {
+		window = glfwCreateWindow( m_WinWidth, m_WinHeight,
+								   "Draw Triangle",
+								   nullptr, nullptr );
+	}
 
-    }
+	void initVulkan()
+	{
+		// TODO: Write Vulkan initialize logic here
+	}
 
-    void cleanup()
-    {
+	void mainLoop()
+	{
+		while ( !glfwWindowShouldClose( window ) )
+		{
+			glfwPollEvents();
+		}
+	}
 
-    }
+	void cleanup()
+	{
+		glfwDestroyWindow( window );
+		glfwTerminate();
+	}
+
+private:
+	const uint32_t m_WinWidth  = 800;
+	const uint32_t m_WinHeight = 600;
+	GLFWwindow* window         = nullptr;
+
 };
 
-int main() 
+int main()
 {
-    HelloTriangleApplication app;
+	HelloTriangleApplication app;
 
-    try
-    {
-        app.run();
-    }
-    catch ( const std::exception& e )
-    {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+	try
+	{
+		app.run();
+	}
+	catch ( const std::exception& e )
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
