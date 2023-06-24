8f8a89df18e3207ca4770c27e453691a663921b1
test <chlcjfwls1@naver.com>
2023-06-24 23:31:33 +0900

    Commit test

--- Vulkan/App.cpp
+++ Vulkan/App.cpp
@@ -15,7 +15,7 @@ void HelloTriangleApplication::run()
 
 void HelloTriangleApplication::initWindow()
 {
-	// These built-in functions the first step to the necessary. 
+	// These built-in functions are the first step to the necessary. 
 	glfwInit();
 	// GLFW was originally desinged to create an OpenGL context.
 	// we need to tell it to not create an OpenGL context with a subsequent call
