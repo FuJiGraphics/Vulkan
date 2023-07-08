97c8822c1ffec298f1cf6ebd47e53ac1fd99b2bd
FuJiGraphics <chlcjfwls1@naver.com>
2023-07-09 00:17:53 +0900

    m

--- Vulkan/App.cpp
+++ Vulkan/App.cpp
@@ -736,6 +736,26 @@ void App::createIndexBuffer()
 
 }
 
+void App::createUniformBuffers()
+{
+    VkDeviceSize bufferSize = sizeof( UniformBufferObject );
+
+    uniformBuffers.resize( MAX_FRAMES_IN_FLIGHT );
+    uniformBuffersMemory.resize( MAX_FRAMES_IN_FLIGHT );
+    uniformBuffersMapped.resize( MAX_FRAMES_IN_FLIGHT );
+
+    for ( size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i )
+    {
+        createBuffer( bufferSize, 
+                      VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+                      VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                      VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, 
+                      uniformBuffers[i],
+                      _mm_haddd_epi8
+                      )
+    }
+}
+
 void App::createCommandBuffers()
 {
     if ( m_CommandBuffers.size() < MAX_FRAMES_IN_FLIGHT )
